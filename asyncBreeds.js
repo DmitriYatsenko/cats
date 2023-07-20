68e9d02e69599847ba4db02dea966105036002b4
Dmitri Yatsenko <dmitriyatsenko1995@gmail.com>
2023-07-20 01:04:34 +0000

    clean up white space

--- asyncBreeds.js
+++ asyncBreeds.js
@@ -1,10 +1,10 @@
 const fs = require('fs');
 
-const breedDetailsFromFile = function(breed, callback) {
+const breedDetailsFromFile = function (breed, callback) {
   console.log('breedDetailsFromFile: Calling readFile...');
   fs.readFile(`./data/${breed}.txt`, 'utf8', (error, data) => {
     console.log('Callback: I have the data!')
-    if (!error) callback(data); 
+    if (!error) callback(data);
     if (error) callback(undefined);
   });
 }
--- syncBreeds.js
+++ syncBreeds.js
@@ -3,7 +3,7 @@ const catBreeds = {
   'Bombay': "The golden eyes and the shiny black coat of the Bombay is absolutely striking. Likely to bond most with one family member, the Bombay will follow you from room to room and will almost always have something to say about what you are doing, loving attention and to be carried around, often on his caregiver's shoulder."
 }
 
-const breedDetails = function(breed) {
+const breedDetails = function (breed) {
   return catBreeds[breed];
 }
 
